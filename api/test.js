cb73c3db9e4a0644a5be7ea847d3e5d657b6297f
omall <omall@localhost>
2026-06-22 02:16:12 +0700

    debug: use module.exports format (CommonJS)

--- api/test.js
+++ api/test.js
@@ -1,15 +1,10 @@
-export default function handler(request) {
-  return new Response(
-    JSON.stringify({
-      message: "Hello from Vercel Functions!",
-      method: request.method,
-      hasSupabaseUrl: !!process.env.VITE_SUPABASE_URL,
-      hasServiceKey: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
-      envKeys: Object.keys(process.env).length,
-    }),
-    {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' }
-    }
-  );
-}
+module.exports = function handler(request, response) {
+  response.status(200).json({
+    message: "Hello from Vercel Functions!",
+    method: request.method,
+    hasSupabaseUrl: !!process.env.VITE_SUPABASE_URL,
+    hasServiceKey: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
+    envKeys: Object.keys(process.env).length,
+    runtime: "node",
+  });
+};
